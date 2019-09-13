70fcbff9fc2879f7ab3d7a2bd4201451fed14728
viktor-oksentiuk-57 <voksentiuk@premierconnect.com.ua>
2019-09-13 19:17:47 +0300

    make seeds

--- database/seeds/BlogCategoriesTableSeeder.php
+++ database/seeds/BlogCategoriesTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class BlogCategoriesTableSeeder extends Seeder
+class  BlogCategoriesTableSeeder  extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,6 +11,25 @@ class BlogCategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //заполняет перед запуском seed
+        $categories = [];
+        $cName = 'Без категории';
+
+        $categories [] = [
+            'title' => $cName,
+            'slug' => str_slug($cName),
+            'parent_id' => 0,
+        ];
+
+        for ( $i = 1; $i < 10; $i++) {
+            $cName = 'Категория'.$i;
+            $parent_id = ($i > 4) ? rand(1, 4) : 1;
+            $categories [] = [
+                'title' => $cName,
+                'slug' => str_slug($cName),
+                'parent_id' => $parent_id,
+            ];
+        }
+        \DB::table('blog_categories')->insert($categories);
     }
 }
